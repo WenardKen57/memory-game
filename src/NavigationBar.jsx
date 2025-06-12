7eee1dc0e94fc6cdfb689cd2fa762fe060be2b0a
Wenard <wenardken57@gmail.com>
2025-06-12 13:28:48 +0800

    Add logo and score board (but not functional yet)

--- src/NavigationBar.jsx
+++ src/NavigationBar.jsx
@@ -1,3 +1,12 @@
 export default function NavigationBar() {
-  return <div className="navigationBar"></div>;
+  const highScore = 100;
+  const currentScore = 0;
+
+  return <div className="navigationBar">
+    <h1>Memory game</h1>
+    <div>
+      <p>High score: {highScore}</p>
+      <p>Current score: {currentScore}</p>
+    </div>
+  </div>;
 }
